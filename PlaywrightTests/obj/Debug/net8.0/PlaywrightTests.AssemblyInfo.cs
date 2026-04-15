53de35d57c68347113b5dc61c6b38747282fe044
DungDA21 <DungDA21@fpt.com>
2026-04-15 16:50:17 +0700

    add file playwright.ps1

--- PlaywrightTests/obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
+++ PlaywrightTests/obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d43ac4d81c4aa636f6958a107c5efffd254f432")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
